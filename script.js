5ecb4fcc5e4a75c939760a5566b80b435421158d
LataBansavade <139533645+LataBansavade@users.noreply.github.com>
2023-08-23 17:55:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,30 +8,16 @@ const person = {
   age: 80,
 };
 
-// Create a reference to the players array
-const team = players;
+let team = players;
+let team1 = [...players];
 
-// Create a copy of the players array using spread syntax
-const team1 = [...players];
-
-// Create a copy of the person object using object destructuring
-const cap1 = { ...person };
-
-// Testing changes
-players[0] = "Mike";
-person.name = "Jane";
-
-console.log("Original players:", players);
-console.log("Team:", team);
-console.log("Team 1:", team1);
-console.log("Original person:", person);
-console.log("Cap1:", cap1);
+let cap1 = {...person};
 
 
-// window.players = players;
-// window.person = person;
-// window.team = team;
-// window.team1 = team1;
-// window.cap1 = cap1;
+window.players = players;
+window.person = person;
+window.team = team;
+window.team1 = team1;
+window.cap1 = cap1;
 
 
